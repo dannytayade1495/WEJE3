5e978e857452d3438569e5dc9eaf071bf8453ef8
dannytayade1495 <dannytayade1495@gmail.com>
2023-04-08 18:38:15 +0530

    adding springrest

---
+++ springrest/src/main/java/com/jspiders/springrest/controller/EmployeeController.java
@@ -0,0 +1,49 @@
+package com.jspiders.springrest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jspiders.springrest.pojo.EmployeePOJO;
+import com.jspiders.springrest.response.EmployeeResponse;
+import com.jspiders.springrest.service.EmployeeService;
+
+@RestController
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeService service;
+	
+	@PostMapping(path = "/add",
+				consumes = {MediaType.APPLICATION_JSON_VALUE},
+				produces = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<EmployeeResponse> addEmployee(@RequestBody EmployeePOJO employee) {
+		EmployeePOJO pojo = service.addEmployee(employee);
+		if (pojo != null) {
+			//Success
+			return new ResponseEntity<EmployeeResponse>(new EmployeeResponse("OK", "Employee data added. ", pojo, null), HttpStatus.ACCEPTED);
+		}
+		//Failure
+		return new ResponseEntity<EmployeeResponse>(new EmployeeResponse("FAIL", "Employee data not added. ", null, null), HttpStatus.BAD_REQUEST);
+	}
+	
+	@GetMapping(path = "/search/{id}",
+			produces = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<EmployeeResponse> searchEmployee(@PathVariable int id){
+		EmployeePOJO pojo = service.searchEmployee(id);
+		if (pojo != null) {
+			//Success
+			return new ResponseEntity<EmployeeResponse>(new EmployeeResponse("OK", "Employee data found. ", pojo, null), HttpStatus.FOUND);
+		}
+		//Failure
+		return new ResponseEntity<EmployeeResponse>(new EmployeeResponse("FAIL", "Employee data not found. ", null, null), HttpStatus.NOT_FOUND);
+	}
+
+}
---
+++ springrest/src/main/java/com/jspiders/springrest/response/EmployeeResponse.java
@@ -0,0 +1,22 @@
+package com.jspiders.springrest.response;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.jspiders.springrest.pojo.EmployeePOJO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class EmployeeResponse {
+	
+	private String status;
+	private String msg;
+	private EmployeePOJO data;
+	private List<EmployeePOJO> list;
+}
